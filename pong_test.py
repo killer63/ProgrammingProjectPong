9d3fe4f08198f221213525c90bfd4f6fddd200f1
killer63 <45488868+killer63@users.noreply.github.com>
2019-05-12 18:02:44 +0300

    Update pong_test.py

--- pong_test.py
+++ pong_test.py
@@ -1,5 +1,3 @@
-# Simple Pong in Python 3 for Beginners
-# By @TokyoEdTech
 
 import turtle
 import os
@@ -21,7 +19,9 @@ class Paddle_A:
     paddle_a.shapesize(stretch_wid=5,stretch_len=1)
     paddle_a.penup()
     paddle_a.goto(-350, 0)
-    
+    global lock
+
+    #move of paddle
     def up():
         y = Paddle_A.paddle_a.ycor()
         y += 20
@@ -33,9 +33,13 @@ class Paddle_A:
         Paddle_A.paddle_a.sety(y)
 
     def move():
-        wn.listen()
-        wn.onkeypress(Paddle_A.up, "w")
-        wn.onkeypress(Paddle_A.down, "s")
+        lock.acquire()
+        try:
+            wn.listen()
+            wn.onkeypress(Paddle_A.up, "w")
+            wn.onkeypress(Paddle_A.down, "s")
+        finally:
+            lock.release()
     
 # Paddle B
 class Paddle_B:
@@ -46,7 +50,9 @@ class Paddle_B:
     paddle_b.shapesize(stretch_wid=5,stretch_len=1)
     paddle_b.penup()
     paddle_b.goto(350, 0)
+    global lock
 
+    #move of paddle
     def up():
         y = Paddle_B.paddle_b.ycor()
         y += 20
@@ -58,9 +64,38 @@ class Paddle_B:
         Paddle_B.paddle_b.sety(y)
 
     def move():
-        wn.listen()
-        wn.onkeypress(Paddle_B.up, "Up")
-        wn.onkeypress(Paddle_B.down, "Down")
+        lock.acquire()
+        try:
+            wn.listen()
+            wn.onkeypress(Paddle_B.up, "Up")
+            wn.onkeypress(Paddle_B.down, "Down")
+        finally:
+            lock.release()
+
+# block
+class Block: 
+    block = turtle.Turtle()
+    block.speed(0)
+    block.shape("square")
+    block.color("white")
+    block.shapesize(stretch_wid=5,stretch_len=1)
+    block.penup()
+    block.goto(0, -200)
+    block.dy = 1
+    global lock
+
+    def move():
+        lock.acquire()
+        try:
+            Block.block.sety(Block.block.ycor() + Block.block.dy)
+            if Block.block.ycor() > 290:
+                Block.block.sety(290)
+                Block.block.dy *= -1
+            elif Block.block.ycor() < -290:
+                Block.block.sety(-290)
+                Block.block.dy *= -1
+        finally:
+            lock.release()
 
 # Ball
 class Ball:
@@ -72,12 +107,14 @@ class Ball:
     ball.goto(0, 0)
     ball.dx = 1
     ball.dy = 1
-
-     # Top and bottom
+    global lock
+     
+     # borders of screen
     def borders():
         if Ball.ball.ycor() > 290:
             Ball.ball.sety(290)
             Ball.ball.dy *= -1
+
         elif Ball.ball.ycor() < -290:
             Ball.ball.sety(-290)
             Ball.ball.dy *= -1
@@ -94,17 +131,25 @@ class Ball:
             Ball.ball.goto(0, 0)
             Ball.ball.dx *= -1
 
+    # move of the ball
     def move():
         Ball.ball.setx(Ball.ball.xcor() + Ball.ball.dx)
         Ball.ball.sety(Ball.ball.ycor() + Ball.ball.dy)
     
-    # Ball collisions
+    # ball collisions
     def collision():
-        if Ball.ball.xcor() < -340 and Ball.ball.ycor() < Paddle_A.paddle_a.ycor() + 50 and Ball.ball.ycor() > Paddle_A.paddle_a.ycor() - 50:
-            Ball.ball.dx *= -1
+        lock.acquire()
+        try:
+            if Ball.ball.xcor() < -340 and Ball.ball.ycor() < Paddle_A.paddle_a.ycor() + 50 and Ball.ball.ycor() > Paddle_A.paddle_a.ycor() - 50:
+                Ball.ball.dx *= -1
 
-        elif Ball.ball.xcor() > 340 and Ball.ball.ycor() < Paddle_B.paddle_b.ycor() + 50 and Ball.ball.ycor() > Paddle_B.paddle_b.ycor() - 50:
-            Ball.ball.dx *= -1
+            elif Ball.ball.xcor() > 340 and Ball.ball.ycor() < Paddle_B.paddle_b.ycor() + 50 and Ball.ball.ycor() > Paddle_B.paddle_b.ycor() - 50:
+                Ball.ball.dx *= -1
+
+            elif Ball.ball.ycor() < Block.block.ycor() + 50 and Ball.ball.ycor() > Block.block.ycor() - 50 and Ball.ball.xcor() == Block.block.xcor():
+                Ball.ball.dx *= -1
+        finally:
+            lock.release()
 
 # Pen
 class Pen:
@@ -128,17 +173,24 @@ class Pen:
         Pen.pen.write("Player A: {}  Player B: {}".format(Pen.score_a, Pen.score_b), align="center", font=("Courier", 24, "normal"))
     
 # Main game loop
-while True:
-    wn.update()
-    # Move the ball
-    p1 = threading.Thread(target=Ball.move(), args=())
-    p2 = threading.Thread(target=Ball.borders(), args=())
-    p3 = threading.Thread(target=Ball.collision(), args=())
-    p4 = threading.Thread(target=Paddle_A.move(), args=())
-    p5 = threading.Thread(target=Paddle_B.move(), args=())
+def main():
+    global lock
+    
+    while True:
+        wn.update()
+        lock = threading.Lock()
+        p1 = threading.Thread(target=Ball.move(), args=())
+        p2 = threading.Thread(target=Ball.borders(), args=())
+        p3 = threading.Thread(target=Ball.collision(), args=())
+        p4 = threading.Thread(target=Paddle_A.move(), args=())
+        p5 = threading.Thread(target=Paddle_B.move(), args=())
+        p6 = threading.Thread(target=Block.move(), args=())
+        p1.start()
+        p2.start()
+        p3.start()
+        p4.start()
+        p5.start()
+        p6.start()
 
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
+# Start a main game loop
+main()
